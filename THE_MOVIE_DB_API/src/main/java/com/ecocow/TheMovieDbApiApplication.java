8f6c50e66f4b5400cf2ced3afeb7dce0abd1b9ef
jungdaehun1024 <pooh99102408@gmail.com>
2024-09-10 01:33:10 +0900

    DB연동 yml파일 설정 후 Git 연결

---
+++ THE_MOVIE_DB_API/src/main/java/com/ecocow/TheMovieDbApiApplication.java
@@ -0,0 +1,13 @@
+package com.ecocow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheMovieDbApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TheMovieDbApiApplication.class, args);
+	}
+
+}
---
+++ THE_MOVIE_DB_API/src/test/java/com/ecocow/TheMovieDbApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecocow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TheMovieDbApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
